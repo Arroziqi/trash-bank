2cbb2cd0ce3e47059c1233b9d35e6b465696656d
Arroziqi <ahmadarroziqi@gmail.com>
2024-11-07 00:44:31 +0700

    waste-category is completed except delete feature

--- src/app/(features)/(admin)/data-master/waste-category/components/table.jsx
+++ src/app/(features)/(admin)/data-master/waste-category/components/table.jsx
@@ -4,10 +4,14 @@ import { useEffect, useState } from "react";
 import Color from "../../../const/color";
 import Cookies from "js-cookie";
 import { API_BASE_URL } from "@/app/const/const";
+import EditModal from "./editModal";
 
 export default function TableWasteCategory({ isDataUpdated }) {
   const [wasteCategory, setWasteCategory] = useState([]);
   const [token, setToken] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [wasteCategorySelected, setWasteCategorySelected] = useState(null);
+  const [wasteCategorySelectedId, setWasteCategorySelectedId] = useState(null);
 
   const fetchData = async () => {
     try {
@@ -59,6 +63,14 @@ export default function TableWasteCategory({ isDataUpdated }) {
     }
   };
 
+  const openModal = (event, category, id) => {
+    if (event?.preventDefault) event.preventDefault();
+
+    setWasteCategorySelected(category);
+    setWasteCategorySelectedId(id);
+    setIsModalOpen((prev) => !prev);
+  };
+
   useEffect(() => {
     const tokenValue = Cookies.get("token");
     setToken(tokenValue);
@@ -69,38 +81,53 @@ export default function TableWasteCategory({ isDataUpdated }) {
   }, [isDataUpdated]);
 
   return (
-    <table className="border-collapse table-auto rounded-ss-lg w-full">
-      <thead className="text-white text-left">
-        <tr style={{ backgroundColor: Color.primary }}>
-          <th className="py-[10px] px-3 border border-black">
-            Kategori Sampah
-          </th>
-          <th className="py-[10px] px-3 border border-black">Aksi</th>
-        </tr>
-      </thead>
-      <tbody>
-        {wasteCategory.map((wasteCategory) => (
-          <tr key={wasteCategory.id}>
-            <td className="py-[10px] px-3 border border-black">
-              {wasteCategory.category}
-            </td>
-            <td className="py-[10px] px-3 border border-black">
-              <div className="flex gap-4">
-                <a href="" className="text-blue-500 underline ">
-                  Edit
-                </a>
-                <a
-                  href=""
-                  onClick={(e) => deleteData(wasteCategory.id, e)}
-                  className="text-red-500 underline "
-                >
-                  Hapus
-                </a>
-              </div>
-            </td>
+    <>
+      <EditModal
+        openModal={isModalOpen}
+        closeModal={(event) => openModal(event)}
+        category={wasteCategorySelected}
+        id={wasteCategorySelectedId}
+        fetchData={fetchData}
+      />
+      <table className="border-collapse table-auto rounded-ss-lg w-full">
+        <thead className="text-white text-left">
+          <tr style={{ backgroundColor: Color.primary }}>
+            <th className="py-[10px] px-3 border border-black">
+              Kategori Sampah
+            </th>
+            <th className="py-[10px] px-3 border border-black">Aksi</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {wasteCategory.map((wasteCategory) => (
+            <tr key={wasteCategory.id}>
+              <td className="py-[10px] px-3 border border-black">
+                {wasteCategory.category}
+              </td>
+              <td className="py-[10px] px-3 border border-black">
+                <div className="flex gap-4">
+                  <a
+                    href=""
+                    onClick={(e) =>
+                      openModal(e, wasteCategory.category, wasteCategory.id)
+                    }
+                    className="text-blue-500 underline "
+                  >
+                    Edit
+                  </a>
+                  <a
+                    href=""
+                    onClick={(e) => deleteData(wasteCategory.id, e)}
+                    className="text-red-500 underline "
+                  >
+                    Hapus
+                  </a>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
